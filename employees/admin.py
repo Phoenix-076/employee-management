23355f774dec9ab4bd363158e3acffa58ae49f38
Phoenix-076 <mongarparjeet@gmail.com>
2025-12-02 15:26:02 +0600

    all missing files

--- employees/admin.py
+++ employees/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
 
-from .models import Employee
-
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = (
-        "first_name",
-        "last_name",
-        "email",
-        "department",
-        "position",
-        "is_active",
-    )
-    list_filter = ("department", "is_active")
-    search_fields = ("first_name", "last_name", "email", "department", "position")
+# Register your models here.
